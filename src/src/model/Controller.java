7d62080ade147c211026d5d1e90dd03234fd930a
jorge2-1 <leongar007@gmail.com>
2026-02-15 18:04:47 -0500

    final

--- src/src/model/Controller.java
+++ src/src/model/Controller.java
@@ -4,11 +4,43 @@ public class Controller {
     ArrayList<User> users =  new ArrayList<>();
 
     public String searchUserForName(String name) {
+        if (users.isEmpty()) {
+            return "Usuario no existentes";
+        }
         for (User user : users) {
-            if (user.getName().equals(name)) {
+            if (user.getName().equalsIgnoreCase(name)) {
                 return user.toString();
             }
         }
         return "No existe el usuario con el nombre : " + name;
+    }
+
+    public String addUser(String name,  String id, String phoneNumber) {
+        User user = new User(name,id,phoneNumber);
+        users.add(user);
+        return "El usuario fue agregado correctamente";
+    }
+
+    public String searchUserForId(String id) {
+        if (users.isEmpty()) {
+            return "Usuario no existentes";
+        }
+        for (User user : users) {
+            if (user.getId().equals(id)) {
+                return user.toString();
+            }
+        }
+        return "No existe el usuario con el identificador : " + id;
+    }
+    public String searchUserForPhoneNumber(String number) {
+        if (users.isEmpty()) {
+            return "Usuario no existentes";
+        }
+        for (User user : users) {
+            if (user.getPhoneNumber().equals(number)) {
+                return user.toString();
+            }
+        }
+        return "No existe el usuario con el numero : " + number;
     }
 }
