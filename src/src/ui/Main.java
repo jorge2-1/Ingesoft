5103b580d693cdd1757379e3633e9438893d2b42
jorge2-1 <leongar007@gmail.com>
2026-02-15 13:41:26 -0500

    Creado la parte de ingreso de registro de los datos del usuario

--- src/src/ui/Main.java
+++ src/src/ui/Main.java
@@ -1,4 +1,28 @@
 package ui;
+import model.Controller;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
+    Controller controller = new Controller();
+    BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Main main = new Main();
+
+    }
+
+    public void CreatedUser(){
+        try {
+            System.out.print("Bienvenido:\n Por favor ingrese los siguentes datos:\n");
+            System.out.print("Nombre completo: ");
+            String name = reader.readLine();
+            System.out.print("Numero de contactos: ");
+            String number = reader.readLine();
+            System.out.print("Numero de identificacion: ");
+            String id = reader.readLine();
+        }catch (Exception e){}
+
+
+
+    }
 }
